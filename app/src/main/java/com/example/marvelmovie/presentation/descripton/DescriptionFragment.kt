629ab696d5f8d02cfd040bbbe6001e273ae0d89c
Ingrid <ingrid.oliveiratk@gmail.com>
2024-08-22 23:36:57 -0300

    ajustando informações da tela de descrição

--- app/src/main/java/com/example/marvelmovie/presentation/descripton/DescriptionFragment.kt
+++ app/src/main/java/com/example/marvelmovie/presentation/descripton/DescriptionFragment.kt
@@ -18,42 +18,28 @@ class DescriptionFragment : Fragment() {
     ): View {
         binding = FragmentDescriptionBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val movieResult: MovieResult = (arguments?.getParcelable<MovieResult>(MOVIE_RESULT)) as MovieResult
+        val movieResult: MovieResult =
+            (arguments?.getParcelable<MovieResult>(MOVIE_RESULT)) as MovieResult
+        descriptionBinding(movieResult)
+        return root
+    }
+
+    private fun descriptionBinding(movieResult: MovieResult) {
         binding.titleMovie.text = movieResult.title
         binding.txtActor.text = movieResult.actors
         binding.txtPlot.text = movieResult.plot
         binding.txtGenre.text = movieResult.genre
-     //   Glide.get().load(url).into(binding.imgMovieDesc)
+        val imgMovie = binding.imgMovieDesc
 
-        return root
-
-//        val intent = intent
-//        val url = intent.getStringExtra("imgMovie")
-
-
-    }
-    private fun descriptionBinding(movieResult: MovieResult)  {
-        binding.txtActor.text = movieResult.title
-
-//        var actor = txtActor
-//        var title = titleMovie
-//        var plot = txtPlot
-//        var genre = txtGenre
-//        var imgMovie = imgMovieDesc
-//
-//        val value = intent.getStringExtra("actor")
-//        val titleDesc = intent.getStringExtra("title")
-//        val plotDesc = intent.getStringExtra("plot")
-//        val genreDesc = intent.getStringExtra("genre")
-//
-//        actor.text = value
-//        title.text = titleDesc
-//        plot.text = plotDesc
-//        genre.text = genreDesc
+        imgMovie.let {
+            Glide.with(imgMovie.context).load(
+                movieResult.poster
+            ).into(it)
+        }
     }
 
     companion object {
-        const val MOVIE_RESULT ="movieResult"
+        const val MOVIE_RESULT = "movieResult"
 
         fun newInstance(movieResult: MovieResult): DescriptionFragment {
             return DescriptionFragment().apply {
@@ -63,5 +49,4 @@ class DescriptionFragment : Fragment() {
             }
         }
     }
-
 }
